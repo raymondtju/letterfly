e26302e34b726eb4b79c9d60f1a0e2443888f8c2
WellyOnGit <justinwellypanvito@gmail.com>
2024-04-16 22:07:16 +0700

    fix welcome.dart from Chrisandy

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:Letterfly/login.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
@@ -12,119 +13,114 @@ class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //  appBar: AppBar(
-      //   centerTitle: true,
-      //   backgroundColor: Colors.black,
-      //   title: Text(
-      //       'E-MAIL FOR NEWBIE',
-      //       style: TextStyle(
-      //         color: Colors.white
-      //       ),
-      //     ),
-      //  ),
-       body: 
-        Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //  appBar: AppBar(
+        //   centerTitle: true,
+        //   backgroundColor: Colors.black,
+        //   title: Text(
+        //       'E-MAIL FOR NEWBIE',
+        //       style: TextStyle(
+        //         color: Colors.white
+        //       ),
+        //     ),
+        //  ),
+        body: Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
             children: [
-              Column(
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      Row(
                         children: [
-                          Row(
-                            children: [
-                              Icon(Icons.mail),
-                              SizedBox(width: 10,),
-                              Text(
-                                'Letterfly',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 24
-                                ),
-                              ),
-                            ],
+                          SvgPicture.asset(
+                            'assets/logo/Logo.svg',
+                            height: 25,
                           ),
-                          Text(
-                            'Akses surat lebih mudah',
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const Text(
+                            'Letterfly',
                             style: TextStyle(
-                              fontWeight: FontWeight.w200,
-                              fontSize: 16
-                            ),
-                          )
+                                fontFamily: "SF",
+                                letterSpacing: -1,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 24),
+                          ),
                         ],
                       ),
                     ],
                   ),
                 ],
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(padding: EdgeInsets.symmetric(vertical: 120)),
-                  Padding(
-                    padding: EdgeInsets.only(right: 120), 
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(right: 20),
-                          child: Text(
-                            'Welcome To Letterfly',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 28
-                            ),
-                          ),
-                        ),
-                        Text(
-                          'Tempat kamu bisa dokumentasi dan sortir suratmu suratmu fleksibel sesuai keinginan.',
-                          style: TextStyle(
-                            fontSize: 12,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-                  OutlinedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context, 
-                        MaterialPageRoute(builder: (context) => const Login()));
-                    }, 
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Start Creating Letter',
-                          style: TextStyle(
-                            color: Colors.white
-                          ),
-                        ),
-                        SizedBox(width: 10,),
-                        Icon(Icons.arrow_right_alt, color: Colors.white,),
-                      ],
-                    ),
-                    style: OutlinedButton.styleFrom(
-                      minimumSize: Size(double.infinity, 50),
-                      backgroundColor: Colors.black,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.zero
-                      ),
-                      side: BorderSide(color: Colors.black, width: 1)
-                    ),
-                  ),
-                  Padding(padding: EdgeInsets.only(bottom: 30))
-                ],
-              ),
             ],
           ),
-        )
-    );
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(padding: EdgeInsets.symmetric(vertical: 120)),
+              const Padding(
+                padding: EdgeInsets.only(right: 120),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 20),
+                      child: Text(
+                        'Welcome To Letterfly',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 28),
+                      ),
+                    ),
+                    Text(
+                      'Tempat kamu bisa dokumentasi dan sortir suratmu suratmu fleksibel sesuai keinginan.',
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+              OutlinedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const Login()));
+                },
+                style: OutlinedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 50),
+                    backgroundColor: Colors.black,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero),
+                    side: const BorderSide(color: Colors.black, width: 1)),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Start Creating Letter',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Icon(
+                      Icons.arrow_right_alt,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(bottom: 30))
+            ],
+          ),
+        ],
+      ),
+    ));
   }
 }
