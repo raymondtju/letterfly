b34cbabe54efefe034acf719a5bde215c1b8a101
WellyOnGit <justinwellypanvito@gmail.com>
2024-04-24 23:40:29 +0700

    Add suratkuasa_view.dart and category_view.dart and a provider for it

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:Letterfly/category/category_view.dart';
+import 'package:Letterfly/category/suratkuasa_view.dart';
 import 'package:Letterfly/forgotPassword.dart';
 import 'package:Letterfly/login.dart';
+import 'package:Letterfly/provider/letterfly_provider.dart';
 import 'package:Letterfly/signup.dart';
 import 'package:Letterfly/successful.dart';
 import 'package:Letterfly/welcome.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:Letterfly/category/categoryGridView.dart';
+import 'package:provider/provider.dart';
 
 main() => runApp(const MyApp());
 
@@ -13,16 +17,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: "/home",
-      routes: {
-        '/home': (context) => const WelcomePage(),
-        '/login': (context) => const LoginPage(),
-        '/signup': (context) => const SignUpPage(),
-        '/sukses': (context) => const SuccessfulPage(),
-        '/forgetPass': (context) => const ForgotPasswordPage(),
-        '/category': (context) => const CategoryGridView(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => LetterFlyProvider()),
+      ],
+      child: MaterialApp(
+        initialRoute: "/home",
+        routes: {
+          '/home': (context) => const WelcomePage(),
+          '/login': (context) => const LoginPage(),
+          '/signup': (context) => const SignUpPage(),
+          '/sukses': (context) => const SuccessfulPage(),
+          '/forgetPass': (context) => const ForgotPasswordPage(),
+          '/category': (context) => const CategoryView(),
+          '/surat_kuasa': (context) => const SuratKuasaView(),
+        },
+      ),
     );
   }
 }
