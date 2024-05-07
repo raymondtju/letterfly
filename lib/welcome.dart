6c47dbc7c7e31e83d9bf951e4b739f701ada3f40
WellyOnGit <justinwellypanvito@gmail.com>
2024-05-07 17:45:36 +0700

    edit welcome.dart and add a new svg image

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -13,102 +13,84 @@ class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        //  appBar: AppBar(
-        //   centerTitle: true,
-        //   backgroundColor: Colors.black,
-        //   title: Text(
-        //       'E-MAIL FOR NEWBIE',
-        //       style: TextStyle(
-        //         color: Colors.white
-        //       ),
-        //     ),
-        //  ),
-        body: Padding(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
+        appBar: AppBar(
+          title: Row(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              SvgPicture.asset(
+                'assets/logo/Logo.svg',
+                height: 25,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              const Text('Letterfly', style: headlineStyle),
+            ],
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Column(
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          SvgPicture.asset(
-                            'assets/logo/Logo.svg',
-                            height: 25,
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          const Text('Letterfly', style: headlineStyle),
-                        ],
-                      ),
-                    ],
+                  SvgPicture.asset(
+                    'assets/images/undraw_software_engineer_re_tnjc 1.svg',
+                    height: 300,
                   ),
+                  const Padding(
+                    padding: EdgeInsets.only(right: 0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 0),
+                          child: Text('Welcome To Letterfly',
+                              style: headlineStyle),
+                        ),
+                        Text(
+                          'The place where you can document and archive your physical mail is flexible and secure!',
+                          style: TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+                  OutlinedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/login");
+                    },
+                    style: OutlinedButton.styleFrom(
+                        minimumSize: const Size(double.infinity, 50),
+                        backgroundColor: Colors.black,
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero),
+                        side: const BorderSide(
+                            color: Color.fromRGBO(40, 42, 45, 1), width: 1)),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Start Creating Letter',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          Icons.arrow_right_alt,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.only(bottom: 30))
                 ],
               ),
             ],
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(padding: EdgeInsets.symmetric(vertical: 120)),
-              const Padding(
-                padding: EdgeInsets.only(right: 120),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(right: 20),
-                      child: Text('Welcome To Letterfly', style: headlineStyle),
-                    ),
-                    Text(
-                      'Tempat kamu bisa dokumentasi dan sortir suratmu suratmu fleksibel sesuai keinginan.',
-                      style: TextStyle(
-                        fontSize: 12,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-              OutlinedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, "/login");
-                },
-                style: OutlinedButton.styleFrom(
-                    minimumSize: const Size(double.infinity, 50),
-                    backgroundColor: Colors.black,
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.zero),
-                    side: const BorderSide(color: Colors.black, width: 1)),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Start Creating Letter',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Icon(
-                      Icons.arrow_right_alt,
-                      color: Colors.white,
-                    ),
-                  ],
-                ),
-              ),
-              const Padding(padding: EdgeInsets.only(bottom: 30))
-            ],
-          ),
-        ],
-      ),
-    ));
+        ));
   }
 }
